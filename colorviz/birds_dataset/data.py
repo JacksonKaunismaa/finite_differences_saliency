Add visualization script, fix normalization issues

colorviz/birds_dataset/data.py
@@ -49,7 +49,9 @@ class ImageDataset(Dataset):
     
     def generate_one(self):
         idx = np.random.randint(len(self))
-        return self.images[idx], self.labels[idx]
+        while (result:=self[idx]) is None:
+            idx = np.random.randint(len(self))
+        return result['image'].numpy().transpose(1,2,0), result['label']
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
@@ -58,8 +60,8 @@ class ImageDataset(Dataset):
         # bad practice, since we are replicating what transform.ToTensor does since that somehow doesn't work well with read_image
         image = read_image(im_name).float()/255.
         label = self.class_name_to_idx[osp.basename(osp.dirname(im_name))]
-        if np.random.randint(0,{'train': 50, 'valid': 2}[self.split]) == 0 and label in range(0,500,83):
-            print("sampled image", im_name, "label", label, "in split", self.split)
+        # if np.random.randint(0,{'train': 50, 'valid': 2}[self.split]) == 0 and label in range(0,500,83):
+        #     print("sampled image", im_name, "label", label, "in split", self.split)
         if hasattr(self, "transform"):
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
@@ -71,11 +73,4 @@ class ImageDataset(Dataset):
         return sample
 
     def implicit_normalization(self, inpt):
-        # since TextureDatasetGenerator.generate_one returns np.uint8 (since it copies from a loaded image), transforms.ToTensor()
-        # will implicitly do a divide by 255. Since we work in [0, 255] space for basically everything, this function is provided
-        # for convenience and should be called just before we pass any tensor into the model. (This means that networks using this
-        # dataset are working in [0, 1] space). The one exception to this is when
-        # you have already called utils.tensorize on the image, which replicates the behaviour of transforms.ToTensor() (but with
-        # better handling of adding dimensions/transposing) and thus will also implicitly do the divide by 255 operation (if the input
-        # type is np.uint8). In summary, always call either utils.tensorize xor this function before passing into the model.
-        return inpt/255.
+        return inpt

colorviz/conv_color/color_regions.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 import colorsys
-import imageio
+# import imageio
 import scipy.ndimage
 import os
 from tqdm import tqdm

colorviz/conv_color/visualizations.py
@@ -4,7 +4,7 @@ from sklearn import decomposition
 from scipy.interpolate import RegularGridInterpolator
 import torch
 import torch.utils.data
-from tqdm import tqdm
+from tqdm import tqdm, trange
 import matplotlib.pyplot as plt
 import warnings
 from collections import defaultdict
@@ -24,17 +24,17 @@ def find_pca_directions(dataset, sample_size, scales, strides, num_components=4)
 # begin by computing pca directions and d_output_d_alphas
     if isinstance(dataset, torch.utils.data.Dataset):
         sample = []
-        for _ in range(sample_size):
+        for _ in trange(sample_size):
             sample.append(dataset.generate_one()[0])
         print(set(x.shape for x in sample))
-        sample = np.asarray(sample).squeeze().astype(np.float16)
+        sample = np.asarray(sample).squeeze().astype(np.float32)
     elif isinstance(dataset, BatchDataset):
         batch_size = dataset._batch_size.numpy()
         samples_needed = sample_size // batch_size
         print("samples need", samples_needed)
         sample = [s[0].numpy() for s, i in zip(dataset, range(samples_needed))]  # use zip to limit the iteration length
         print("samples list done")
-        sample = np.concatenate(sample).astype(np.float16)
+        sample = np.concatenate(sample).astype(np.float32)
         print("did the sample fine")
     else:
         raise NotImplemented(f"Unknown dataset type {type(dataset)}")