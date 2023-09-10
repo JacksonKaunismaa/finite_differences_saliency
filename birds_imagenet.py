Add split_channels to pca computation, fix bugs in pca computation, relocate OurEfficientNet to network.py, add SiLU to guided backprop, update plt_grid_figure, add image_grid, fix plotting of image intensity graphs

birds_imagenet.py
@@ -16,6 +16,7 @@ from colorviz.birds_dataset.data import ImageDataset
 from colorviz.conv_color.config_objects import ImageDatasetCfg, ExperimentConfig
 from colorviz.conv_color import training
 from colorviz.conv_color import utils
+from colorviz.birds_dataset import network
 
 torch.backends.cudnn.benchmark = True
 
@@ -28,70 +29,6 @@ def main(rank, args):
         torch.cuda.set_device(rank)  # so that nccl knows we are only using that specific device
         print("hi from", rank)
     device = f"cuda:{rank}" if torch.cuda.is_available() else "cpu"
-
-    class OurEfficientNet(nn.Module):
-        def __init__(self, net: EfficientNet, path: str, exp_cfg: ExperimentConfig, n_classes: int):
-            super().__init__()
-            self.model = net
-
-            if not exp_cfg.full:
-                for param in self.model.parameters():
-                    param.requires_grad = False
-            
-            self.path = path
-            self.cfg = exp_cfg
-            self.best_acc = float("-inf")
-
-            if exp_cfg.fc_layers:
-                net.classifier[1] = nn.Sequential(nn.Linear(net.classifier[1].in_features, exp_cfg.fc_layers[0]),
-                                                nn.ReLU(),
-                                                nn.Linear(exp_cfg.fc_layers[0], n_classes)
-                                                )
-            else:
-                net.classifier[1] = nn.Linear(net.classifier[1].in_features, n_classes)
-
-        def forward(self, x):
-            return self.model(x)
-
-        def maybe_save_model_state_dict(self, new_loss=None, new_acc=None, path=None, optim=None, sched=None):
-            if new_acc is not None and new_acc <= self.best_acc:
-                return
-
-            if path is None:
-                path = self.path
-
-            #path = os.path.join("models", path)
-            if new_acc is not None:
-                self.best_acc = new_acc
-            save_dict = {"model_sd": self.state_dict(),
-                            "config": self.cfg,
-                            #"dset_config": self.dset_config,
-                            "best_acc": self.best_acc}
-            if optim is not None:
-                save_dict["optim_sd"] = optim.state_dict()
-            if sched is not None:
-                save_dict["sched_sd"] = sched.state_dict()
-            torch.save(save_dict, path)
-
-        def load_model_state_dict(net, optim=None, sched=None):
-                print("Found path of", net.path)
-                load_dict = torch.load(net.path)
-
-                if optim is not None:
-                    optim.load_state_dict(load_dict["optim_sd"])
-                if sched is not None:
-                    sched.load_state_dict(load_dict["sched_sd"])
-
-                net.load_state_dict(load_dict["model_sd"])
-
-        def train(self, mode: bool=True):
-            print(".train() called")
-            for module in self.model.modules():
-                if isinstance(module, nn.Linear):
-                    print("set only this guy to", mode)
-                    module.train(mode)
-                else:  # ie. force all other layers to be in evaluation mode except for the classifier
-                    module.train(False)
 
     # lr schedule
     # simplify the task
@@ -132,7 +69,7 @@ def main(rank, args):
                                 full=False,
                                 fc_layers=[2_000])
 
-    net = OurEfficientNet(efficientnet_b0(weights=EfficientNet_B0_Weights.IMAGENET1K_V1),
+    net = network.OurEfficientNet(efficientnet_b0(weights=EfficientNet_B0_Weights.IMAGENET1K_V1),
                         "./checkpoint/efficientnet_birds.dict", exp_cfg, dsets['train'].num_classes)
 
     # net.eval() # set batchnorms to inference mode (this would just get erased during evaluation anyway)

colorviz/birds_dataset/data.py
@@ -46,20 +46,24 @@ class ImageDataset(Dataset):
 
     def __len__(self):
         return len(self.image_names)
+
+    def load_raw(self, idx):
+        im_name = self.image_names[idx]
+        # bad practice, since we are replicating what transform.ToTensor does since that somehow doesn't work well with read_image
+        image = read_image(im_name).float()/255.
+        label = self.class_name_to_idx[osp.basename(osp.dirname(im_name))]
+        return image, label
     
     def generate_one(self):
         idx = np.random.randint(len(self))
         while (result:=self[idx]) is None:
             idx = np.random.randint(len(self))
-        return result['image'].numpy().transpose(1,2,0), result['label']
+        return result['image'].numpy().transpose(1,2,0), result['label'], idx
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        im_name = self.image_names[idx]
-        # bad practice, since we are replicating what transform.ToTensor does since that somehow doesn't work well with read_image
-        image = read_image(im_name).float()/255.
-        label = self.class_name_to_idx[osp.basename(osp.dirname(im_name))]
+        image,label = self.load_raw(idx)
         # if np.random.randint(0,{'train': 50, 'valid': 2}[self.split]) == 0 and label in range(0,500,83):
         #     print("sampled image", im_name, "label", label, "in split", self.split)
         if hasattr(self, "transform"):

colorviz/conv_color/hooks.py
@@ -133,7 +133,7 @@ class GuidedBackprop(HookAdder):
     def relu_backward_hook_creater(self, name):
         #print("potentiall adding to", name)
         def _relu_backward_hook(module, grad_in, grad_out):
-            if not isinstance(module, nn.ReLU) or name in self.exceptions:  # only consider ReLUs
+            if not isinstance(module, (nn.ReLU, nn.SiLU)) or name in self.exceptions:  # only consider ReLUs
                 return
             #print("guided_backprop on", name)
             #print("R^{l+1} is", grad_in[0], abs(grad_in[0]).max())
@@ -149,9 +149,10 @@ class GuidedBackprop(HookAdder):
 
     def relu_forward_hook_creater(self, name):
         def _relu_forward_hook(module, inpt, outpt):
-            if not isinstance(module, nn.ReLU) or name in self.exceptions:
+            if not isinstance(module, (nn.ReLU, nn.SiLU)) or name in self.exceptions:
                 return
             self.forward_relu_outputs.append(outpt)
+        return _relu_forward_hook
 
 def set_initializers(mod, gain):
     has_sub_mods = False

colorviz/conv_color/utils.py
@@ -52,17 +52,30 @@ def find_good_ratio(size): # find ratio as close to a square as possible
     height = size//width
     return min(height, width), max(height, width)  # height, width
 
+def image_grid(img_list):  # plot a grid of images, with img_list being flat
+    height, width = find_good_ratio(len(img_list))
+    for i in range(height):
+        for j in range(width):
+            idx = i*width + j + 1
+            plt.subplot(height, width, idx)
+            plt.imshow(img_list[idx])
+            remove_borders(plt.gca())
 
-def plt_grid_figure(inpt_grid, titles=None, colorbar=True, cmap=None, transpose=False, hspace=-0.4, first_cmap=None, channel_mode=None):
+
+def plt_grid_figure(inpt_grid, col_titles=None, colorbar=True, cmap=None, transpose=False, hspace=-0.4, 
+                    first_cmap=None, channel_mode=None, row_titles=None, crop_to=None, zero_centered_cmap=True):
+    # plot a grid of images (inpt grid must be in the shape you want the images to display in)
+    # inpt_grid.shape == (N_vert, N_horiz, img_height, img_width)
     #np_grid = np.array(grid).squeeze()
     #if len(np_grid.shape) != 4:
     #    np_grid = np.expand_dims(np_grid, 0)
     #if transpose:
     #    np_grid = np_grid.transpose(1,0,2,3)
-    if not isinstance(inpt_grid[0], list):
+    if not isinstance(inpt_grid[0], (list, np.ndarray)):
         inpt_grid = [inpt_grid]
     if cmap is None:
         cmap = "bwr"
+        
 
     nrows = len(inpt_grid[0]) if transpose else len(inpt_grid)
     if channel_mode not in ["split", "collapse"] and not isinstance(channel_mode, int) and channel_mode is not None:
@@ -70,48 +83,58 @@ def plt_grid_figure(inpt_grid, titles=None, colorbar=True, cmap=None, transpose=
 
     if channel_mode == "split":
         grid = [[] for _ in range(nrows)]
-        expanded_titles = []
+        expanded_col_titles = []
         for i, row in enumerate(inpt_grid):
             for j, img in enumerate(row):
                 idx = (j,i) if transpose else (i,j)
-                #print("on idx", i,j)
+                # print("on idx", idx, "img_shape", img.shape)
                 if idx[1] != 0 and img.ndim == 3:  # not in first column
-                    #print("decided to do some expanding, curr_len", len(grid[idx[0]]))
+                    # print("decided to do some expanding, curr_len", len(grid[idx[0]]))
                     expanded_view = [channel_view for channel_view in img.transpose(2,0,1)]
-                    #print("expandend_amount", len(expanded_view))
+                    # print("expandend_amount", len(expanded_view), len(grid[idx[0]]))
                     grid[idx[0]] += expanded_view  # ie. assume HWC
+                    # print("len(grid[idx[0]])", len(grid[idx[0]]))
                     if idx[0] == 0:  # in first row
                         #print("expanding titles also", len(titles), j, len(row))
-                        expanded_titles += [f"Channel {c} {titles[idx[1]]}" for c in range(len(expanded_view))]
+                        expanded_col_titles += [f"Channel {c} {col_titles[idx[1]] if col_titles else ''}" for c in range(len(expanded_view))]
                 else:
                     grid[idx[0]].append(img)
-                    if idx[0] == 0:
-                        expanded_titles.append(titles[idx[1]])
-        titles = expanded_titles
+                    if idx[0] == 0 and col_titles:
+                        expanded_col_titles.append(col_titles[idx[1]])
+        col_titles = expanded_col_titles
     else:
         grid = inpt_grid
 
     #print([len(x) for x in grid])
-    im_size = grid[0][0].shape[0]
+
+    im_size = crop_to if crop_to else grid[0][0].shape[0]
+
     ncols = len(grid) if transpose and not channel_mode == "split" else len(grid[0])
 
     #print(expanded_titles, len(expanded_titles))
-    print(nrows, ncols, im_size)
+    print(f"{nrows=}, {ncols=}, {im_size=}")
     fig = plt.figure(figsize=(4/128*im_size*ncols, 5/128*im_size*nrows))
     gridspec = fig.add_gridspec(nrows, ncols, hspace=hspace)
     axes = gridspec.subplots(sharex="col", sharey="row")
     if len(axes.shape) == 1:
         axes = np.expand_dims(axes, 0)
-    print(axes.shape)
+    print(f"{axes.shape=}")
     for i, row in enumerate(grid):
         for j, unsqueezed_img in enumerate(row):
             img = unsqueezed_img.squeeze()
+            if crop_to is not None:
+                # print(img.shape)
+                im_actual_size = img.shape[0]
+                crop_amt = (im_actual_size - crop_to)//2
+                img = img[crop_amt:-crop_amt, crop_amt:-crop_amt]
+                # print(img.shape, crop_amt, im_actual_size, i, j)
             idx = (j,i) if transpose and not channel_mode == "split" else (i,j)  # split_channels already accounts for transpose
             axes[idx].set_xticks([])
             axes[idx].set_yticks([])
             remove_borders(axes[idx])
             if idx[1] == 0: # assume explain_img is the first thing
                 if len(img.shape) == 3 and img.shape[2] == 3:
+                    # print(img.shape)
                     im = axes[idx].imshow(img)
                 else:
                     if first_cmap is None:
@@ -125,13 +148,19 @@ def plt_grid_figure(inpt_grid, titles=None, colorbar=True, cmap=None, transpose=
 
                 img_max = np.max(abs(img))
                 if cmap != "gray":
-                    im = axes[idx].imshow(img, cmap=cmap, interpolation="nearest", vmax=img_max, vmin=-img_max)
+                    # print(img.sum())
+                    if zero_centered_cmap:
+                        im = axes[idx].imshow(img, cmap=cmap, vmax=img_max, vmin=-img_max) # interpolation='nearest'
+                    else:
+                        im = axes[idx].imshow(img, cmap=cmap)
                 else:
                     axes[idx].imshow(img, cmap=cmap)
                 if colorbar:
                     plt.colorbar(im, pad=0, fraction=0.048)
-            if titles and idx[0] == 0:
-                axes[idx].set_title(titles[idx[1]])
+            if col_titles and idx[0] == 0:
+                axes[idx].set_title(col_titles[idx[1]])
+            if row_titles and idx[1] == 0:
+                axes[idx].set_ylabel(row_titles[idx[0]])
     #plt.show()
 
 

colorviz/conv_color/visualizations.py
@@ -12,6 +12,8 @@ from scipy.optimize import minimize
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import LightSource
 from tensorflow.python.data.ops.dataset_ops import BatchDataset
+import multiprocessing as mp
+
 
 from .hooks import AllActivations
 from . import utils
@@ -20,8 +22,8 @@ from . import training
 # PCA Stuff
 
 
-def find_pca_directions(dataset, sample_size, scales, strides, num_components=4):
-# begin by computing pca directions and d_output_d_alphas
+def get_sample_for_pca(sample_size, dataset):
+    # begin by computing pca directions and d_output_d_alphas
     if isinstance(dataset, torch.utils.data.Dataset):
         sample = []
         for _ in trange(sample_size):
@@ -38,8 +40,13 @@ def find_pca_directions(dataset, sample_size, scales, strides, num_components=4)
         print("did the sample fine")
     else:
         raise NotImplemented(f"Unknown dataset type {type(dataset)}")
-    im_size = sample.shape[1]
-    im_channels = sample.shape[-1]
+    return sample
+
+def find_pca_directions(dataset, scales, strides, sample=None, num_components=4, split_channels=False):
+    if isinstance(sample, int):  # if a sample is not pre-provided, 'sample' is treated as the sample size to use
+        sample = get_sample_for_pca(sample, dataset)
+    sample_size = sample.shape[0]  # sample is [N, H, W, C]
+    im_channels = sample.shape[-1] if sample.ndim == 4 else 1
 
     if isinstance(strides, int):
         strides = [strides]*len(scales)
@@ -47,94 +54,46 @@ def find_pca_directions(dataset, sample_size, scales, strides, num_components=4)
     print("Got sample, beginning directions")
     pca_direction_grids = []
     for scale, stride in zip(scales, strides):
-        windows = np.lib.stride_tricks.sliding_window_view(sample, (scale,scale), axis=(1,2))
-        strided_windows = windows[:, ::stride, ::stride, :]  # [N, H, W, C]
+        windows = np.lib.stride_tricks.sliding_window_view(sample, (scale,scale), axis=(1,2)) 
+        strided_windows = windows[:, ::stride, ::stride, :]  # [N, abs_posx, abs_posy, C?, within_windowx, within_windowy]
 
-        xs = np.mgrid[scale:im_size:stride]  # technically wrong (but its shape is correct)
-        num_grid = xs.shape[0]
-        pca_direction_grid = np.zeros((num_grid, num_grid, num_components, scale, scale, im_channels))
-
+        pca_direction_grid = np.zeros((strided_windows.shape[1], strided_windows.shape[2], 
+                                       num_components, scale, scale, im_channels))
         pca_fitter = decomposition.PCA(n_components=num_components, copy=False)
         scale_fitter = StandardScaler()
-        for i in tqdm(range(num_grid)):
-            for j in range(num_grid):
-                pca_selection = strided_windows[:, i, j, :]
-                flattened = pca_selection.reshape(sample_size, -1)
-                normalized = scale_fitter.fit_transform(flattened)
-                with warnings.catch_warnings():
-                    warnings.simplefilter("ignore")  # gives pointless zero-division warnings
-                    pca_fitter.fit(normalized)
-                for comp in range(num_components):
-                    pca_direction_grid[i, j, comp] = pca_fitter.components_[comp].reshape(scale, scale, im_channels)
+        for i in tqdm(range(strided_windows.shape[1])):
+            for j in range(strided_windows.shape[2]):
+                for c in range(im_channels):                          
+                    if strided_windows.ndim == 6:  # ie. like [N, abs_posx, abs_posy, C, within_windowx, within_windowy]
+                        if split_channels:
+                            pca_selection = strided_windows[:, i, j, c] # [N, within_windowx, within_windowy]
+                        else:
+                            pca_selection = strided_windows[:, i, j].transpose(0,2,3,1) #[N, within_windowx, within_windowy, C]
+                    elif strided_windows.ndim == 5: # ie. like [N, abs_posx, abs_posy, within_windowx, within_windowy]
+                        pca_selection = strided_windows[:, i, j]  # [N, within_windowx, within_windowy]
+                    flattened = pca_selection.reshape(sample_size, -1)
+                    normalized = scale_fitter.fit_transform(flattened)
+                    with warnings.catch_warnings():
+                        warnings.simplefilter("ignore")  # gives pointless zero-division warnings
+                        pca_fitter.fit(normalized)
+                    for comp in range(num_components):
+                        if split_channels:
+                            pca_direction_grid[i, j, comp, ..., c] = pca_fitter.components_[comp].reshape(scale, scale)
+                        else:
+                            pca_direction_grid[i, j, comp] = pca_fitter.components_[comp].reshape(scale, scale, im_channels)
+                    if not split_channels:  # if split_channels=False, we don't want to iterate over c
+                        break
+                    
 
         pca_direction_grids.append(pca_direction_grid.copy())
     return pca_direction_grids
 
-def old_old_pca_direction_grids(model, dataset, target_class, img,
-                        sample_size=512, scales=[3,5,9,15],
-                        strides=None, pca_direction_grids=None,
-                       gaussian=False):
-    # begin by computing pca directions and d_output_d_alphas
-    model.eval()
-    im_size = dataset.cfg.size
-    if strides is None:
-        strides = scales
-    if pca_direction_grids is None:
-        pca_direction_grids = find_pca_directions(dataset, sample_size, scales, strides)
-    d_out_d_alpha_grids = []
-    interpolators = []
-    indices_grid = np.mgrid[0:im_size, 0:im_size]
-
-    for s, (scale, stride) in enumerate(zip(scales, strides)):
-        index_window = np.lib.stride_tricks.sliding_window_view(indices_grid, (scale,scale), axis=(1,2))
-        strided_indices = index_window[:, ::stride, ::stride]
-
-        xs = np.mgrid[0:im_size-scale:stride]
-        num_grid = xs.shape[0]
-        #print(xs, num_grid)
-        d_out_d_alpha_grid = np.zeros((num_grid, num_grid))
-        for i in tqdm(range(num_grid)):
-            for j in range(num_grid):
-                # get pca direction for current patch
-                pca_direction = pca_direction_grids[s][i,j,0].transpose(2,0,1)
-                indices = strided_indices[:, i, j, ...]  # will have to slice these
-
-                # do d_output_d_alpha computation
-                alpha = torch.tensor(0.0, requires_grad=True).to(dataset.cfg.device)
-                direction_tensor = torch.tensor(pca_direction).to(dataset.cfg.device).float().unsqueeze(0)
-                img_tensor = torch.tensor(img.transpose(2,0,1)).to(dataset.cfg.device).float().unsqueeze(0)
-                img_tensor[..., indices[0,0,0]:indices[0,-1,-1]+1, indices[1,0,0]:indices[1,-1,-1]+1] += alpha*direction_tensor
-                output = model(img_tensor)[0,target_class]
-                d_out_d_alpha = torch.autograd.grad(output, alpha)[0]
-
-                d_out_d_alpha_grid[i,j] = d_out_d_alpha.detach().cpu().numpy()
-        d_out_d_alpha_grids.append(d_out_d_alpha_grid.copy())
-        interpolators.append(RegularGridInterpolator((xs+scale//2, xs+scale//2), d_out_d_alpha_grid,
-                                                     bounds_error=False, fill_value=None))
-    #print(d_out_d_alpha_grids[-1], d_out_d_alpha_grids[-1].shape)
-    # now, per pixel, interpolate what the d_output_d_alpha value would be if the window
-    # were centered at that pixel, then take the max over all possible scales
-    saliency_map = np.zeros_like(img)
-    scale_wins = [0] * len(scales)
-    for i in tqdm(range(im_size)):
-        for j in range(im_size):
-            best_d_out_d_alpha = 0
-            best_scale = -1
-            for s in range(len(scales)):
-                interp_value = interpolators[s]([i,j])
-                if abs(interp_value) >= abs(best_d_out_d_alpha):
-                    best_d_out_d_alpha = interp_value
-                    best_scale = s
-            saliency_map[i,j] = best_d_out_d_alpha
-            scale_wins[best_scale] += 1
-    print(scale_wins)
-    return saliency_map
 
 def pca_direction_grids(model, dataset, target_class, img, scales, pca_direction_grids,
                         strides=None, gaussian=False, component=0, batch_size=32):
     # begin by computing d_output_d_alphas
     model.eval()
-    im_size = dataset.cfg.size
+    im_size = img.shape[0]
     if strides is None:
         strides = scales
     if isinstance(strides, int):
@@ -297,7 +256,7 @@ def finite_differences_map(model, dataset, target_class, img, unfairness="fair",
                            batch_size=32, num_values=20):
     # generate a saliency map using finite differences method (iterate over colors)
     model.eval()
-    im_size = dataset.cfg.size
+    im_size = img.shape[0]
     #img = img.astype(np.float32)/255. # normalization handled later
     indices = np.mgrid[:im_size, :im_size].transpose(1,2,0).reshape(im_size*im_size, -1)
     stacked_img = np.repeat(np.expand_dims(img, 0), batch_size, axis=0)
@@ -862,7 +821,7 @@ def random_pixels_response(net, dataset, num_pixels, img_id=987_652, one_class=T
     generated_img, lbl, color, size, pos  = dataset.generate_one()
     tensor_img = utils.tensorize(generated_img, device=dataset.cfg.device)
 
-    im_size = dataset.cfg.size
+    im_size = generated_img.shape[0]
     possible_pixels = np.mgrid[:im_size, :im_size].transpose(1,2,0).reshape(im_size*im_size, -1)
     selected_pixels = possible_pixels[np.random.choice(len(possible_pixels), num_pixels, replace=False)]
     num_inside = 0
@@ -907,7 +866,7 @@ def circle_pixels_response(net, dataset, num_pixels, width, img_id=987_652, oute
     possible_indices[:,0] = (pos[0][0] + np.cos(angle)*radii[:,None]).flat
     possible_indices[:,1] = (pos[0][1] + np.sin(angle)*radii[:,None]).flat
     possible_indices = np.round(possible_indices).astype(np.int64)  # all possible pixels we can select (cant do np.unique because slow
-    im_size = dataset.cfg.size
+    im_size = generated_img.shape[0]
     zero_img = np.zeros((im_size, im_size))
     #print(possible_indices.shape)
     zero_img[possible_indices[:,0], possible_indices[:,1]] = 1